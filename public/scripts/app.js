dbf62168d25cc31ef2efe9058d32c10757a89138
kcdporter <kiracdporter@gmail.com>
2019-03-19 20:39:31 +0000

    added a warning color change of 10 characters for counter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,15 +3,3 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// $('.tweet-form').on('keyup', function(event) {
-//   let remaining = 140 - $(this).val().length;
-//   console.log(remaining);
-//   console.log('this:', this)
-//   let counter = $(this).silbings('.counter');
-//   console.log(counter)
-//   counter.text(remaining);
-//   // $(counter) = `$(remaining) + ' Characters left');
-//   // if (remaining < 0) {
-//   //   $('.counter').css('color', 'red');
-  
-// });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,21 @@
 $('document').ready(function() {
   console.log('Document is ready')
+
 }); 
 
 $('.tweet-form').on('keyup', function(event) {
   let remaining = 140 - $(this).val().length;
   let counter = $(this).siblings('.counter');
+  console.log(this)
   counter.text(remaining + ' Characters left');
-  if (remaining < 0) {
-    $('.counter').css('color', 'red');
+  if (remaining > 10) {
+    counter.removeClass('limit-reached warning');
+  } else {
+    if (remaining < 0){
+      counter.removeClass('warning').addClass('limit-reached');
+    } else {
+      counter.removeClass('limit-reached').addClass('warning');
+    }
   }
+
 });
