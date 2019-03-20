7427e01429add45d29d40709f43deea818df0cbc
kcdporter <kiracdporter@gmail.com>
2019-03-20 23:27:13 +0000

    built an alert to stop posts that are empty, or over character length limit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,50 +1,3 @@
-const data = [
-  {
-    'user': {
-      'name': 'Newton',
-      'avatars': {
-        'small':   'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
-        'regular': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
-        'large':   'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png'
-      },
-      'handle': '@SirIsaac'
-    },
-    'content': {
-      'text': 'If I have seen further it is by standing on the shoulders of giants'
-    },
-    'created_at': 1461116232227
-  },
-  {
-    'user': {
-      'name': 'Descartes',
-      'avatars': {
-        'small':   'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png',
-        'regular': 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png',
-        'large':   'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png'
-      },
-      'handle': '@rd' },
-    'content': {
-      'text': 'Je pense , donc je suis'
-    },
-    'created_at': 1461113959088
-  },
-  {
-    'user': {
-      'name': 'Johann von Goethe',
-      'avatars': {
-        'small':   'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png',
-        'regular': 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png',
-        'large':   'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png'
-      },
-      'handle': '@johann49'
-    },
-    'content': {
-      'text': 'Es ist nichts schrecklicher als eine tätige Unwissenheit.'
-    },
-    'created_at': 1461113796368
-  }
-];
-
 $(document).ready(function(){
   function renderTweets(data) {
     for (let tweet of data){
@@ -70,21 +23,34 @@ $(document).ready(function(){
     return $article;
   }
 
+
   
   $('#tweet-form').submit(function(event) {
     event.preventDefault();
     console.log("default prevented");  
     const $serializedTweet = $(this).serialize();
-    console.log($serializedTweet)
-
-    $.ajax({
-      method: 'POST',
-      url: '/tweets',
-      data: $serializedTweet
-    })
+    const length = $(this)[0][0].value.length;
+  
+    if(length === 0){
+      alert("Please be a bit more verbose!")
+    } else {
+      if (length <= 140){
+        $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: $serializedTweet,
+        })
+      } else {
+      alert("Please be less verbose!")
+      }
+    }
   })
 
-  loadTweets(){
-    
+  function loadTweets(){
+    $.get('/tweets', function(data){
+      console.log(data)
+      renderTweets(data);
+    })
   }
+  loadTweets();
 });
