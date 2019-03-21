89ce16120475800cf7ab41df5b916009baa3a72f
kcdporter <kiracdporter@gmail.com>
2019-03-21 01:58:24 +0000

    refactored the tweet building section of app.js, added flag, seperated validation code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
 
 // functions pertaining to building and viewing tweets
+  //This function we need to invoke immediately once our page gets loaded.
+  loadTweets();
+
+  function createTweetElement(tweet) {
+    let $header = ($('<header>')
+    .append($('<img>').addClass('avatar').attr('src', tweet.user.avatars.large))
+    .append($('<h2>').addClass('name').text(tweet.user.name))
+    .append($('<h6>').addClass('handle').text(tweet.user.handle))
+    );
+    let $body = ($('<p>').text(tweet.content.text));
+    let $footer = ($('<footer>')
+    .append($('<div>').text(tweet.created_at))
+    .append($('<div>').addClass('icons').text('🇨🇦 ❤️ ↻')));
+    let $article = ($('<article>').addClass('tweet')
+    .append($header)
+    .append($body)
+    .append($footer));
+    return $article;
+  }
+
   function renderTweets(data) {
     for (let tweet of data){
       let $tweet = createTweetElement(tweet);
@@ -8,65 +28,51 @@ $(document).ready(function(){
     }
   }
 
-  function createTweetElement(tweet) {
-    let $header = ($('<header>')
-        .append($('<img>').addClass('avatar').attr('src', tweet.user.avatars.large))
-        .append($('<h2>').addClass('name').text(tweet.user.name))
-        .append($('<h6>').addClass('handle').text(tweet.user.handle))
-        );
-    let $body = ($('<p>').text(tweet.content.text));
-    let $footer = ($('<footer>')
-          .append($('<div>').text(tweet.created_at))
-          .append($('<div>').addClass('icons').text('🇨🇦 ❤️ ↻')));
-    let $article = ($('<article>').addClass('tweet')
-            .append($header)
-            .append($body)
-            .append($footer));
-    return $article;
-  }
-  
- $('#tweet-form').submit(function(event) {
-    event.preventDefault();
-    console.log("default prevented");  
-    const $serializedTweet = $(this).serialize();
-    const length = $(this)[0][0].value.length;
-    if(length === 0){
-      alert("Please be a bit more verbose!")
-    } else {
-      if (length <= 140){
-        $.ajax({
-        method: 'POST',
-        url: '/tweets',
-        data: $serializedTweet,
-        success: function() {
-          postTweets();
-        }})
-      } else {
-      alert("Please be less verbose!")
-      }
-    }
-  });
-
   function loadTweets(){
     $.get('/tweets', function(data){
       renderTweets(data);
     })
   }
-  loadTweets();
   
-  function postTweets () {
-    $.ajax('/tweets', {
-    method: 'GET',
-    success: function(data) {
-      renderTweets(data);
+
+  $('#tweet-form').submit(function(event) {
+    event.preventDefault();
+    console.log("default prevented");  
+    const $serializedTweet = $(this).serialize();
+    const length = $(this)[0][0].value.length;
+
+    let flag = true;
+    //validations start here
+    if(length === 0){
+      alert("Please be a bit more verbose!")
+      flag = false;
+    } else if (length > 140){
+       alert("Please be a bit less verbose!");
+       flag = false;
     }
-    })
-  };
+
+    if(flag){
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: $serializedTweet,
+        success: function(result) {
+          console.log("tweet was posted successfully");
+          loadTweets();
+        },
+        error: function(err){
+          console.log("there was an error posting the tweet");
+        }
+      })
+    }
+  });
+  
 
   // Functions relating to CSS/ JQuery Animations
   $('#compose').click(function() {
     if( $('.container').is(':hidden')){
-      $('.container').slideDown("slow")
+      $('.container').slideDown("slow");
+      $('textarea').focus();
     } else {
       $('.container').hide();
     }
